19e33dab52bd5c83de79d1363de534b95a62d473
Hajinjing <yk234589@naver.com>
2022-08-08 14:25:41 +0900

    게시글 상세조회 페이지, 게시글 수정 페이지, 게시글 수정, 게시글 삭제 컨트롤러

--- src/main/java/com/mega/board/controller/BoardController.java
+++ src/main/java/com/mega/board/controller/BoardController.java
@@ -5,9 +5,7 @@ import com.mega.board.service.BoardService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -41,6 +39,38 @@ public class BoardController {
     @PostMapping("/post")
     public String write(BoardDto boardDto) {
         boardService.savePost(boardDto);
+
+        return "redirect:/";
+    }
+
+    // 게시글 상세조회 페이지
+    @GetMapping("/post/{no}")
+    public String detail(@PathVariable("no") Long no, Model model) {
+        BoardDto boardDto = boardService.getPost(no);
+
+        model.addAttribute("boardDto", boardDto);
+        return "board/detail.html";
+    }
+
+    // 게시글 수정 페이지
+    @GetMapping("/post/edit/{no}")
+    public String edit(@PathVariable("no") Long no, Model model) {
+        BoardDto boardDto = boardService.getPost(no);
+
+        model.addAttribute("boardDto", boardDto);
+        return "board/update.html";
+    }
+
+    @PutMapping("/post/edit/{no}")
+    public String update(BoardDto boardDto) {
+        boardService.savePost(boardDto);
+
+        return "redirect:/";
+    }
+
+    @DeleteMapping("/post/{no}")
+    public String delete(@PathVariable("no") Long no) {
+        boardService.deletePost(no);
 
         return "redirect:/";
     }
